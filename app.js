ff4ca8f722053c52bd95805c36094f4476083f68
Swetha <swetha.s2022cce@sece.ac.in>
2024-03-10 21:54:27 +0530

    portfolio

---
+++ app.js
@@ -0,0 +1,19 @@
+const header=document.querySelector("header")
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY>120);
+});
+
+
+let menu=document.querySelector('#menu-icon');
+let nav=document.querySelector('.navbar');
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
+
+
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    nav.classList.remove('active')
+}
